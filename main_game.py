e93648a9a28be81539dfb872a4aed571d3b98cf9
istiakAHMEDsaad <istik2020@gmail.com>
2025-02-28 19:35:01 +0600

    surface & blit

--- main_game.py
+++ main_game.py
@@ -1,13 +1,27 @@
 import pygame
 from sys import exit
 
+
 pygame.init()
 
+# initial display size
 screen = pygame.display.set_mode((800, 400))
+# set game title
+pygame.display.set_caption('Jumpy Jumpy')
+# set game frame rate
+clock = pygame.time.Clock()
+
+# surface size (H, W)
+test_surface = pygame.Surface((100, 80))
+# surface color
+test_surface.fill('Red')
 
 while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
             exit()
+    # surface position (x, y)
+    screen.blit(test_surface,(0,0))
     pygame.display.update()
+    clock.tick(60)
